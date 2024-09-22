eb65de0db98724df21a2eba5d9453dd1f21a0e2a
Aiden Lee <yw010804@gmail.com>
2024-09-22 10:42:43 +0900

    feat: setting

---
+++ src/main/java/com/hy/hongyeon/HongYeonApplication.java
@@ -0,0 +1,13 @@
+package com.hy.hongyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongYeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongYeonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hy/hongyeon/HongYeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.hongyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongYeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
