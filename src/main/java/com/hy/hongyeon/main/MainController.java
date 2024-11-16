d846c036b099f585282733a71cf0f3120753b8da
Aiden Lee <yw010804@gmail.com>
2024-11-16 12:23:58 +0900

    Feat/layout (#10)

--- src/main/java/com/hy/hongyeon/main/MainController.java
+++ src/main/java/com/hy/hongyeon/main/MainController.java
@@ -1,10 +1,19 @@
 package com.hy.hongyeon.main;
 
+import com.hy.hongyeon.event.entity.Event;
+import com.hy.hongyeon.event.service.EventService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class MainController {
+    private final EventService eventService;
+
     // 루트 URL을 /hongyeon으로 리다이렉트
     @GetMapping("/")
     public String redirectToHongyeon() {
@@ -12,7 +21,10 @@ public class MainController {
     }
 
     @GetMapping("/hongyeon")
-    public String  main(){
+    public String  main(Model model){
+        List<Event> eventList = this.eventService.findAll();
+        model.addAttribute("eventList", eventList);
+
         return "home/home";
     }
 }
